2e93b81d8cc1fe8b7097be30771ef67b7249e30e
root <root@p663-bastion.p663.cecc.ihost.com>
2021-10-22 08:53:09 -0400

    Initial TFServing stub

---
+++ deploy-model-with-tfserving/build_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+image_name=quay.io/ibm/kubeflow-component-deploy-model-with-tfserving
+image_tag=latest
+full_image_name=${image_name}:${image_tag}
+
+cd "$(dirname "$0")"
+docker build -t "${full_image_name}" .
+docker push "${full_image_name}"
+
